a011c17bb4a82951ba438f3fd0b4d2527b8a44a1
fedemarty <fede.a.marty@gmail.com>
2023-06-20 12:03:21 -0300

    Se agrega Route de Categoría

---
+++ routes/categoriaRoutes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import CategoriaController from "../Controllers/CategoriaController.js";
+
+const categoriaRoutes = Router();
+
+const categoriaController = new CategoriaController();
+
+categoriaRoutes.get("/", categoriaController.getAllCategorias);
+categoriaRoutes.get("/:id", categoriaController.getCategoriaById);
+categoriaRoutes.post("/", categoriaController.createCategoria);
+categoriaRoutes.delete("/:id", categoriaController.deleteCategoria);
+gastoRoutes.put("/:id", categoriaController.updateCategoria);
+
+export default categoriaController;
